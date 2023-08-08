dc92092076dca8374a2cb309af57d3237e3d9b28
Xevenst <stevenjoentoro@gmail.com>
2023-08-08 16:51:31 +0800

    Portfolio semi finished

---
+++ Javascript/Portfolio.js
@@ -0,0 +1,22 @@
+function printHelloThreeTimes(){
+    // const fileList = document.getElementById('output');
+
+    //     fetch('\\Assets\\Documents\\PDF\\')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             for (const filePath of data) {
+    //                 const listItem = document.createElement('li');
+    //                 listItem.textContent = filePath;
+    //                 fileList.appendChild(listItem);
+    //             }
+    //         })
+    //         .catch(error => console.error('Error:', error));
+
+    // for(let i=0;i<9;i++){
+    //     const getDocuments = document.getElementById('output');
+    //     getDocuments.innerHTML += '<a href="..\\Assets\\Documents\\PDF\\Steven Jonathan ATS Resume.pdf"><img src="..\\Assets\\Images\\For Resume\\Steven Jonathan ATS Resume.png" style="width: 300px;height: 300px;"></a>';
+    // }
+}
+
+// Call the function when the page loads
+window.onload = printHelloThreeTimes;
